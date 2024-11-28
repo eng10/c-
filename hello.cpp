b0031ccb4f06708e6688936f68d06fd604757d3f
eng10 <cabdilahi0099@gmail.com>
2024-11-28 05:58:26 +0300

    done

---
+++ hello.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "ku soo dhawoow adeega hubaal sarvice" << endl;
+    cout << "gali pnikaaga" << endl;
+    int pin;
+    cin >> pin;
+    int number;
+    int dir;
+    int numberSend;
+
+    cout << "hubaal bank" << endl;
+    cout << "1.Eeg hadhaagaaga" << endl;
+    cout << "2.lacag dirid" << endl;
+    cout << "3.lacag qaadasho" << endl;
+    cout << "4.tixraac" << endl;
+    cout << "5.beled pinka" << endl;
+
+    cout << "gali pinkaaga" << endl;
+    cin >> number;
+
+    if (number == 1)
+    {
+        cout << "hadhaagaagu waa 10,00";
+    }
+    if (number == 2)
+    {
+        cout << "";
+        cin >> dir;
+        cin >> numberSend;
+        cout <<"ma hubtaa ina u dirtya numberSend";
+    }
+    if (number == 3)
+    {
+        cout << "hadhaagaagu waa 10,00";
+    }
+    if (number == 4)
+    {
+        cout << "hadhaagaagu waa 10,00";
+    }
+
+    int number1;
+    int number2;
+    int number3;
+    int number4;
+    int number5;
+
+    number1 = 1;
+    number1 = 2;
+    number1 = 3;
+    number1 = 4;
+    number1 = 5;
+}
